35c17e16b7edfe3806272d042e24c3596094675c
Rodolfo Ripado <ggaspaio@gmail.com>
2012-07-17 09:45:40 +0200

    update

--- manifests/manifest.pp
+++ manifests/manifest.pp
@@ -19,14 +19,24 @@ node 'default' {
     ensure => "present"
   }
 
+  # MySQL
+  class { 'mysql': }
+  class { 'mysql::server':
+      config_hash => {'root_password' => 'vagrant'}
+  }
+  
+
   # Ensure php cli & apache
   class {'php':}
   class {'php::apache2':
     memory_limit => '256M',
   }
+
   class {'php::pear':}
   class {'php::pecl':
     xdebug_remotehost => '192.168.33.1'
   }
+
+
 }
 
