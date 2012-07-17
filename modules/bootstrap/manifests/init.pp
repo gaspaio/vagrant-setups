19040c76609c9cd5037e199f0ef8884131f056f2
Rodolfo Ripado <ggaspaio@gmail.com>
2012-07-17 21:56:22 +0200

    update php module. Ensure apt-get update runs before anything else.

--- modules/bootstrap/manifests/init.pp
+++ modules/bootstrap/manifests/init.pp
@@ -16,27 +16,18 @@ class bootstrap {
   }
 
   # Set FQDN for virtualbox
-  if $virtual == "virtualbox" and $fqdn == '' {
-    $fqdn = "localhost"
+  #TODO We can't assign this variable (in another namepace)
+  #if $::virtual == "virtualbox" and $::fqdn == '' {
+  #  $::fqdn = "localhost"
+  #}
+
+
+  # Ensure apt-get update runs before any other package installation
+  # cf. http://johnleach.co.uk/words/771/puppet-dependencies-and-run-stages
+  exec { "apt-update":
+    command => "/usr/bin/apt-get update"
   }
 
-  # make sure the packages are up to date before beginning
-  exec { "apt-get update":
-    command => "/usr/bin/apt-get update",
-  }
-
-  # because puppet command are not run sequentially, ensure that packages are
-  # up to date before installing before installing packages, services, files, etc.
-  Package {
-    require => Exec["apt-get update"]
-  }
-
-  File {
-    require => Exec["apt-get update"]
-  }
-
-  # Base packages
-  package { 'make':
-    ensure => present,
-  }
+  # Ensure apt-get update has been run before installing any packages
+  Exec["apt-update"] -> Package <| |>
 }
