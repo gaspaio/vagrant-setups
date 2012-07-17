61cb8902ef87d03b3753840ca9af8bd0b619db4a
Rodolfo Ripado <ggaspaio@gmail.com>
2012-07-17 22:35:32 +0200

    php update. example manifest update.

--- manifests/manifest.pp
+++ manifests/manifest.pp
@@ -1,5 +1,7 @@
-node 'default' {
+node 'default' { 
   
+  Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
   include bootstrap
   
   class {'apache':
@@ -7,22 +9,20 @@ node 'default' {
     httpd_group => 'vagrant',
   }
 
-  apache::vhost { 'mysite.test.vt':
-    port               => '80',
-    docroot            => '/var/projects/mysite.test.vt',
-    serveradmin        => 'rodolfo@me.org',
-    configure_firewall => false,
-  }
+  #apache::vhost { 'vhost_name':
+  #  port               => '80',
+  #  docroot            => 'docroot',
+  #  serveradmin        => 'rodolfo@me.org',
+  #  configure_firewall => false,
+  #}
 
-  # The apache module provides us with a new type: a2mod
-  a2mod { 'rewrite':
-    ensure => "present"
-  }
+  a2mod { 'rewrite': ensure => "present" }
+  a2mod { 'expires': ensure => "present" }
 
   # MySQL
   class { 'mysql': }
   class { 'mysql::server':
-      config_hash => {'root_password' => 'vagrant'}
+    config_hash => {'root_password' => 'vagrant'}
   }
   
 
@@ -31,12 +31,14 @@ node 'default' {
   class {'php::apache2':
     memory_limit => '256M',
   }
+  # Ensure MySQL bindings for PHP
+  class {'mysql::php':}
 
   class {'php::pear':}
   class {'php::pecl':
     xdebug_remotehost => '192.168.33.1'
   }
 
-
-}
+  include other
+ }
 
