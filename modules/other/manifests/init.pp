672fc1f0fa6eb9f2db313b8534875b0c91c3f0eb
Rodolfo Ripado <ggaspaio@gmail.com>
2012-07-19 18:19:24 +0200

    removed old Hg install code.

--- modules/other/manifests/init.pp
+++ modules/other/manifests/init.pp
@@ -1,12 +1,5 @@
 class other {
-  $packages = ["htop", "curl", "tmux", "vim", "mercurial"]
-
-  file { "hgrc":
-    path => "/home/vagrant/.hgrc",
-    ensure => present,
-    source => "puppet:///modules/other/hgrc",
-    require => Package['mercurial'],
-  }
+  $packages = ["htop", "curl", "tmux", "vim"]
 
   package { $packages:
     ensure => present,
